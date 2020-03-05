e2f9fdec9abbfc4a066b76a1a370671b4fe3aa3f
zt1994 <zt191610942@163.com>
2020-03-05 20:59:21 +0800

    yield 方法

--- src/main/java/com/multithread/chapter03/SleepThread.java
+++ src/main/java/com/multithread/chapter03/SleepThread.java
@@ -10,6 +10,15 @@ public class SleepThread {
 
     /**
      * 每个线程休眠互不影响 Thread.sleep 只导致当前线程进入指定时间的休眠
+     * <p>
+     * 使用TimeUnit来代替sleep
+     * <p>
+     * 表示休眠3小时24分钟17秒88毫秒的两种方式：
+     * Thread.sleep(12257088L);
+     * TimeUnit.HOURS.sleep(3);
+     * TimeUnit.MINUTES.sleep(24);
+     * TimeUnit.SECONDS.sleep(17);
+     * TimeUnit.MILLISECONDS.sleep(88);
      *
      * @param args
      */
