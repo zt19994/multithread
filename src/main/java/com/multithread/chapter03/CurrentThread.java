11ac2758df0a6b9f9f359de5ead3a71ed8ad39c7
zt1994 <zt191610942@163.com>
2020-03-09 21:16:06 +0800

    获取当前线程

---
+++ src/main/java/com/multithread/chapter03/CurrentThread.java
@@ -0,0 +1,24 @@
+package com.multithread.chapter03;
+
+/**
+ * 当前线程
+ * <p>
+ * currentThread 用于返回当前执行线程的引用
+ *
+ * @author zt1994 2020/3/9 21:13
+ */
+public class CurrentThread {
+
+    public static void main(String[] args) {
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread() == this);
+            }
+        };
+        thread.start();
+
+        String name = Thread.currentThread().getName();
+        System.out.println("main".equals(name));
+    }
+}
