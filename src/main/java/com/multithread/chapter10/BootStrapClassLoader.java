9ae5b1ba43dab6a53f504e93eca95c4b6190cbca
zt1994 <zt191610942@163.com>
2020-04-26 21:25:35 +0800

    类加载器

---
+++ src/main/java/com/multithread/chapter10/BootStrapClassLoader.java
@@ -0,0 +1,21 @@
+package com.multithread.chapter10;
+
+/**
+ * 根类加载器测试
+ * <p>
+ * jvm内置三大类加载器
+ * 1、根类加载器
+ * 2、扩展类加载器
+ * 3、系统类加载器
+ *
+ * @author zt1994 2020/4/26 21:06
+ */
+public class BootStrapClassLoader {
+
+    public static void main(String[] args) {
+        //String.class的类加载器时根加载器，根加载器是获取不到引用的，因此输出为null
+        System.out.println("BootStrap:" + String.class.getClassLoader());
+        // 根加载器所在路径
+        System.out.println(System.getProperty("sun.boot.class.path"));
+    }
+}
---
+++ src/main/java/com/multithread/chapter10/ExtClassLoader.java
@@ -0,0 +1,14 @@
+package com.multithread.chapter10;
+
+/**
+ * 扩展类加载器
+ *
+ * 扩展类加载器的父类是根加载器，主要用于加载JAVA_HOME下的jre\lb\ext子目录里面的类库。
+ * @author zt1994 2020/4/26 21:11
+ */
+public class ExtClassLoader {
+
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("java.ext.dirs"));
+    }
+}
