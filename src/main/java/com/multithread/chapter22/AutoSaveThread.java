79303924d8bff056b986d10c5336602b4b0faa30
zt1994 <zt191610942@163.com>
2020-06-02 22:12:07 +0800

    balking 设计模式

---
+++ src/main/java/com/multithread/chapter22/AutoSaveThread.java
@@ -0,0 +1,31 @@
+package com.multithread.chapter22;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 自动保存线程
+ *
+ * @author zt1994 2020/6/2 21:34
+ */
+public class AutoSaveThread extends Thread {
+
+    private final Document document;
+
+    public AutoSaveThread(Document document) {
+        super("DocumentAutoSaveThread");
+        this.document = document;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                document.save();
+                TimeUnit.SECONDS.sleep(1);
+            } catch (IOException | InterruptedException e){
+                break;
+            }
+        }
+    }
+}
---
+++ src/main/java/com/multithread/chapter22/BalkingTest.java
@@ -0,0 +1,13 @@
+package com.multithread.chapter22;
+
+/**
+ * balking 设计模式测试
+ *
+ * @author zt1994 2020/6/2 21:55
+ */
+public class BalkingTest {
+
+    public static void main(String[] args) {
+        new DocumentEditThread("E:\\", "balking.txt").start();
+    }
+}
