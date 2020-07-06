004318d09bfe223933412487e8dc7864a6154be9
zt1994 <zt191610942@163.com>
2020-07-06 21:19:27 +0800

    文件监控事件

---
+++ src/main/java/com/multithread/chapter28/FileChangeTest.java
@@ -0,0 +1,23 @@
+package com.multithread.chapter28;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * 文件改变测试
+ *
+ * @author zt1994 2020/7/6 21:14
+ */
+public class FileChangeTest {
+
+    public static void main(String[] args) throws Exception {
+        ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.
+                newFixedThreadPool(Runtime.getRuntime().availableProcessors() * 2);
+
+        final EventBus eventBus = new AsyncEventBus(executor);
+        // 注册
+        eventBus.register(new FileChangeListener());
+        DirectoryTargetMonitor monitor = new DirectoryTargetMonitor(eventBus, "E:\\monitor");
+        monitor.startMonitor();
+    }
+}
